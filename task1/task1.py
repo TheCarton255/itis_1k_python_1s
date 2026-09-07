ae0308c9bd8e94ba1c9699c0481f702620819d53
TheCarton255 <pulatovrenat574@gmail.com>
2026-09-07 19:28:40 +0300

    Добавил задачу 1

--- task1/task1.py
+++ task1/task1.py
@@ -1,6 +1,8 @@
 import math
-L = int(input('Введите значение L'))
 g = 9.81
-pi = math.pi 
-T = 2 * pi * math.sqrt(L / g)
-print(T)
+pi = math.pi
+print('L    T')
+for i in range(1, 21, 2):
+	L=i / 10
+	T = 2 * pi * math.sqrt(L / g)
+	print(f'{L:.1f}m {T:.4f}c')
